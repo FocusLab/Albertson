8968ea65fb7fbe1573fbfb21e5b802c2b3287963
Sean O'Connor <sean@focuslab.io>
2012-02-16 00:24:14 -0500

    Added some project skeleton bits.

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError, err:
+    from distutils.core import setup
+
+from albertson import VERSION
+
+setup(
+    name='Albertson',
+    version=".".join(map(str, VERSION)),
+    description="Easy to use, scalable, counters powered by Amazon's DynamoDB",
+    author="Sean O'Connor",
+    author_email="sean@focuslab.io",
+    url="https://github.com/FocusLab/Albetson",
+    packages=['albertson'],
+    license="BSD",
+    long_description=open('README.md').read(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
