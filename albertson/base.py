dc6c2bdbc5a579f671046312b010cb8e5c3d8b0f
Sean O'Connor <sean@focuslab.io>
2012-02-16 15:47:55 -0500

    Added code for determiing the table name to use.

--- albertson/base.py
+++ albertson/base.py
@@ -5,12 +5,23 @@ class Counter(object):
     '''
     Base counter class that can be used directly or overwritten as needed.
     '''
+    table_name = None
 
-    def __init__(self, aws_access_key=None, aws_secret_key=None):
+    def __init__(self, aws_access_key=None, aws_secret_key=None, table_name=None):
         self.conn = self.get_conn(aws_access_key, aws_secret_key)
+        self.table_name = table_name or self.table_name
+
+        super(Counter, self).__init__()
 
     def get_conn(self, aws_access_key=None, aws_secret_key=None):
         return boto.connect_dynamodb(
             aws_access_key_id=aws_access_key,
             aws_secret_access_key=aws_secret_key,
         )
+
+    def get_table_name(self):
+        if not self.table_name:
+            raise NotImplementedError(
+                'You must provide a table_name value or override the get_table_name method'
+            )
+        return self.table_name
--- tests/base.py
+++ tests/base.py
@@ -7,10 +7,43 @@ from albertson.base import Counter
 
 class BaseCounterTests(unittest.TestCase):
 
+    def get_counter(self, counter_class=None, **kwargs):
+        counter_class = counter_class or Counter
+        real_kwargs = {
+            'aws_access_key': config['aws']['access_key'],
+            'aws_secret_key': config['aws']['secret_key'],
+            'table_name': 'albertson-test',
+        }
+        real_kwargs.update(kwargs)
+
+        return counter_class(**real_kwargs)
+
     def test_base_init(self):
-        counter = Counter(
-            aws_access_key=config['aws']['access_key'],
-            aws_secret_key=config['aws']['secret_key'],
-        )
+        counter = self.get_counter()
 
         counter.conn.list_tables()
+
+    def test_get_init_table_name(self):
+        counter = self.get_counter()
+
+        expected = 'albertson-test'
+        result = counter.get_table_name()
+
+        self.assertEquals(expected, result)
+
+    def test_get_empty_table_name(self):
+        counter = self.get_counter(table_name='')
+
+        with self.assertRaises(NotImplementedError):
+            counter.get_table_name()
+
+    def test_get_attr_table_name(self):
+        class TestCounter(Counter):
+            table_name = 'some_name'
+
+        counter = self.get_counter(counter_class=TestCounter, table_name=None)
+
+        expected = 'some_name'
+        result = counter.get_table_name()
+
+        self.assertEquals(expected, result)
